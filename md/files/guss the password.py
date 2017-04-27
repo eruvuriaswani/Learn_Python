4723df55563679bd449397c4c416092ed7e9c79a
Mayank Johri <funmayank@yahoo.co.in>
2017-04-27 08:35:49 +0530

    trying md files

---
+++ md/files/guss the password.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#x=int(input('Enter a passkeyid: '))
+#print(type(x))
+#while x != 10:
+#    print(x),
+#    x=int(input('Enter a passkeyid: '))
+#key=input("Press key...")
+#
+#
+#x=input('Enter a passkeyid: ')
+#print(type(x))
+#while x != str(10):
+#    print(x),
+#    x=input('Enter a passkeyid: ')
+#key=input("Press key...")
+
+x=input('Enter a passkeyid: ')
+print(type(x))
+while x != "abc":
+    print(x),
+    x=input('Enter a passkeyid: ')
+key=input("Press key...")

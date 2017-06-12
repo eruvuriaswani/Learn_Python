0c427aaa57a2e3e1e80751409458f7bb77c18fc0
Mayank Johri <funmayank@yahooco.in>
2017-06-12 12:21:26 +0530

    Removed the project as it was taking too much space

--- project/aat2 (copy 1)/run.py
+++
@@ -1,6 +0,0 @@
-# run.py
-
-from apps import app
-
-if __name__ == "__main__":
-    app.run(host='localhost', port=8081, debug=True)
--- project/aat_1/start.py
+++
@@ -1,6 +0,0 @@
-# run.py
-
-from apps import app
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)

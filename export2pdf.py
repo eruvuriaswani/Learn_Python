3df563352ee82bdf0f1a26fc48efb211bc8d19b7
Mayank Johri <funmayank@yahoo.co.in>
2017-07-22 14:56:34 +0530

    TEST

--- export2pdf.py
+++ export2pdf.py
@@ -4,14 +4,16 @@ import subprocess
 FORMAT = "pdf"
 
 
-def exportPdf(srcFile):
+def export_Pdf(srcFile):
+    """."""
+
     # folder = srcFile
     cmd = "jupyter nbconvert --to %s \"%s\"" % (FORMAT, srcFile)
     print(cmd)
     print(subprocess.Popen(cmd, stdout=subprocess.PIPE).stdout.read())
 
-
 lstFiles = []
-for filename in glob.iglob('**/*.ipynb', recursive=True):
+for filename in glob.iglob('**/*.ipynb'):
+    # , recursive=True):
     lstFiles.append(filename)
-    exportPdf(filename)
+    export_Pdf(filename)

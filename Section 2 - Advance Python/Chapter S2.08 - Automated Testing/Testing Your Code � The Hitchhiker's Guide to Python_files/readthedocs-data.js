f1dd008f9fb447a2c21fb03d97e9353cfce3d838
Mayank Johri <funmayank@yahoo.co.in>
2017-09-13 07:44:26 +0530

    %*

---
+++ Section 2 - Advance Python/Chapter S2.08 - Automated Testing/Testing Your Code � The Hitchhiker's Guide to Python_files/readthedocs-data.js
@@ -0,0 +1,14 @@
+  var READTHEDOCS_DATA = {
+    project: "python-guide",
+    version: "latest",
+    language: "en",
+    subprojects: {},
+    canonical_url: "http://docs.python-guide.org/en/latest/",
+    theme: "alabaster",
+    builder: "sphinx",
+    docroot: "/docs/",
+    source_suffix: ".rst",
+    api_host: "https://readthedocs.org",
+    commit: "32c55c03"
+  };
+  
